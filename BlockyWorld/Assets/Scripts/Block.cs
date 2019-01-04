0ee4654fd7f96a6a36a5bf1cb5c6c668529bb903
Dominik <36657497+D0miH@users.noreply.github.com>
2019-01-04 18:27:06 +0100

    refactored the way block meshes are added to the chunk mesh

--- BlockyWorld/Assets/Scripts/Block.cs
+++ BlockyWorld/Assets/Scripts/Block.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Block {
 
     // define the face sides of the block
-    public enum FaceDirections { up, down, east, west, north, south };
+    public enum FaceDirection { up, down, east, west, north, south };
 
     // the position of the block in chunk coordinates
     public Vector3 blockPos;
@@ -24,102 +24,64 @@ public class Block {
     /// <param name="chunkMesh">The given chunk mesh</param>
     /// <returns></returns>
     public void AddBlockToChunk(ref ChunkMesh chunkMesh) {
-        AddBlockFaceUp(ref chunkMesh);
-        AddBlockFaceDown(ref chunkMesh);
-        AddBlockFaceEast(ref chunkMesh);
-        AddBlockFaceWest(ref chunkMesh);
-        AddBlockFaceNorth(ref chunkMesh);
-        AddBlockFaceSouth(ref chunkMesh);
-    }
-
-
-    /// <summary>
-    /// Adds the data for the up-facing face of the block to the chunk mesh.
-    /// </summary>
-    /// <param name="chunkMesh">The reference to the chunk mesh that is going to be manipulated</param>
-    void AddBlockFaceUp(ref ChunkMesh chunkMesh) {
-        // calculate the vertices of the face
-        Vector3 point1 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper left back point of cube
-        Vector3 point2 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper right back point
-        Vector3 point3 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper right front point
-        Vector3 point4 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper left front point
-
-        // add them to the mesh
-        chunkMesh.AddFace(point1, point2, point3, point4);
+        AddBlockFace(FaceDirection.up, ref chunkMesh);
+        AddBlockFace(FaceDirection.down, ref chunkMesh);
+        AddBlockFace(FaceDirection.east, ref chunkMesh);
+        AddBlockFace(FaceDirection.west, ref chunkMesh);
+        AddBlockFace(FaceDirection.north, ref chunkMesh);
+        AddBlockFace(FaceDirection.south, ref chunkMesh);
     }
 
     /// <summary>
-    /// Adds the data for the down-facing face of the block to the chunk mesh.
+    /// Adds the face which faces the given direction to the given chunk mesh. 
     /// </summary>
+    /// <param name="direction">The direction of the face</param>
     /// <param name="chunkMesh">The reference to the chunk mesh that is going to be manipulated</param>
-    void AddBlockFaceDown(ref ChunkMesh chunkMesh) {
-        // calculate the vertices of the face
-        Vector3 point1 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower left front point
-        Vector3 point2 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower right front point
-        Vector3 point3 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower right back point
-        Vector3 point4 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // upper left front point
+    void AddBlockFace(FaceDirection direction, ref ChunkMesh chunkMesh) {
+        Vector3 point1;
+        Vector3 point2;
+        Vector3 point3;
+        Vector3 point4;
+        // assign the points depending on the directio of the face
+        switch (direction) {
+            case FaceDirection.south:
+                point1 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper left front point
+                point2 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper right front point
+                point3 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower right front point
+                point4 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower left front point
+                break;
+            case FaceDirection.north:
+                point1 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper right back point
+                point2 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper left back point
+                point3 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower left back point
+                point4 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower right back poin
+                break;
+            case FaceDirection.west:
+                point1 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper left back point
+                point2 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper left front point
+                point3 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower left front point
+                point4 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower left back point
+                break;
+            case FaceDirection.east:
+                point1 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper right front point
+                point2 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper right back point
+                point3 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower right back point
+                point4 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower right front poin
+                break;
+            case FaceDirection.down:
+                point1 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower left front point
+                point2 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower right front point
+                point3 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower right back point
+                point4 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // upper left front point
+                break;
+            default: // default case is to add the upper face
+                point1 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper left back point
+                point2 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper right back point
+                point3 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper right front point
+                point4 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper left front point
+                break;
+        }
 
-        // add them to the mesh
-        chunkMesh.AddFace(point1, point2, point3, point4);
-    }
-
-    /// <summary>
-    /// Adds the data for the east-facing face of the block to the chunk mesh.
-    /// </summary>
-    /// <param name="chunkMesh">The reference to the chunk mesh that is going to be manipulated</param>
-    void AddBlockFaceEast(ref ChunkMesh chunkMesh) {
-        // calculate the vertices of the face
-        Vector3 point1 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper right front point
-        Vector3 point2 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper right back point
-        Vector3 point3 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower right back point
-        Vector3 point4 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower right front point
-
-        // add them to the mesh
-        chunkMesh.AddFace(point1, point2, point3, point4);
-    }
-
-    /// <summary>
-    /// Adds the data for the west-facing face of the block to the chunk mesh.
-    /// </summary>
-    /// <param name="chunkMesh">The reference to the chunk mesh that is going to be manipulated</param>
-    void AddBlockFaceWest(ref ChunkMesh chunkMesh) {
-        // calculate the vertices of the face
-        Vector3 point1 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper left back point
-        Vector3 point2 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper left front point
-        Vector3 point3 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower left front point
-        Vector3 point4 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower left back point
-
-        // add them to the mesh
-        chunkMesh.AddFace(point1, point2, point3, point4);
-    }
-
-    /// <summary>
-    /// Adds the data for the north-facing face of the block to the chunk mesh.
-    /// </summary>
-    /// <param name="chunkMesh">The reference to the chunk mesh that is going to be manipulated</param>
-    void AddBlockFaceNorth(ref ChunkMesh chunkMesh) {
-        // calculate the vertices of the face
-        Vector3 point1 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper right back point
-        Vector3 point2 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z + 0.5f); // upper left back point
-        Vector3 point3 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower left back point
-        Vector3 point4 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z + 0.5f); // lower right back point
-
-        // add them to the mesh
-        chunkMesh.AddFace(point1, point2, point3, point4);
-    }
-
-    /// <summary>
-    /// Adds the data for the south-facing face of the block to the chunk mesh.
-    /// </summary>
-    /// <param name="chunkMesh">The reference to the chunk mesh that is going to be manipulated</param>
-    void AddBlockFaceSouth(ref ChunkMesh chunkMesh) {
-        // calculate the vertices of the face
-        Vector3 point1 = new Vector3(blockPos.x - 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper left front point
-        Vector3 point2 = new Vector3(blockPos.x + 0.5f, blockPos.y + 0.5f, blockPos.z - 0.5f); // upper right front point
-        Vector3 point3 = new Vector3(blockPos.x + 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower right front point
-        Vector3 point4 = new Vector3(blockPos.x - 0.5f, blockPos.y - 0.5f, blockPos.z - 0.5f); // lower left front point
-
-        // add them to the mesh
         chunkMesh.AddFace(point1, point2, point3, point4);
     }
 }
